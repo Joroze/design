eca1f920901e8fdcf3a8029c75a18e7dd615c5a3
Jordan Rosenberg <joroze@Jordans-MacBook-Pro.local>
2020-12-02 14:59:30 -0500

    Update mobile styling for top nav menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import './App.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faSearch, faUser, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+
 import Home from 'pages/Home/Home';
 
+
 function App() {
+
   return (
     <div className="component-app">
       <header className="app-nav">
         <div>
           <nav>
             <ul>
-              <li>
+              <li className='desktop-visible'>
                 <a href='#'>Products</a>
               </li>
-              <li>
+              <li className='desktop-visible'>
                 <a href='#'>Sale</a>
               </li>
-              <li>
+              <li className='desktop-visible'>
                 <a href='#'>Search</a>
+              </li>
+              <li className='mobile-visible'>
+                <a href='#'><FontAwesomeIcon size='lg' icon={faBars} /></a>
+              </li>
+              <li className='mobile-visible'>
+                <a href='#'><FontAwesomeIcon size='lg' icon={faSearch} /></a>
               </li>
             </ul>
           </nav>
@@ -25,14 +36,20 @@ function App() {
                 </h2>
           <nav>
             <ul>
-              <li>
+              <li className='desktop-visible'>
                 <a href='#'>Wishlist</a>
               </li>
-              <li>
+              <li className='desktop-visible'>
                 <a href='#'>Account</a>
               </li>
-              <li>
+              <li className='desktop-visible'>
                 <a href='#'>Shopping Bag (0)</a>
+              </li>
+              <li className='mobile-visible'>
+                <a href='#'><FontAwesomeIcon size='lg' icon={faUser} /></a>
+              </li>
+              <li className='mobile-visible'>
+                <a href='#'><a href='#'><FontAwesomeIcon size='lg' icon={faShoppingBag} /> (0)</a></a>
               </li>
             </ul>
           </nav>
