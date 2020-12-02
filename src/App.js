57ffcd06f1b543cee3004febeabb30bae6e900ab
Jordan Rosenberg <joroze@Jordans-MacBook-Pro.local>
2020-12-02 13:36:53 -0500

    Fix styling

--- src/App.js
+++ src/App.js
@@ -6,35 +6,37 @@ function App() {
   return (
     <div className="component-app">
       <header className="app-nav">
-        <nav>
-          <ul>
-            <li>
-              <a href='#'>Products</a>
-            </li>
-            <li>
-              <a href='#'>Sale</a>
-            </li>
-            <li>
-              <a href='#'>Search</a>
-            </li>
-          </ul>
-        </nav>
-        <h2 className='title'>
-          Sculpture
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a href='#'>Products</a>
+              </li>
+              <li>
+                <a href='#'>Sale</a>
+              </li>
+              <li>
+                <a href='#'>Search</a>
+              </li>
+            </ul>
+          </nav>
+          <h2 className='title'>
+            Sculpture
                 </h2>
-        <nav>
-          <ul>
-            <li>
-              <a href='#'>Wishlist</a>
-            </li>
-            <li>
-              <a href='#'>Account</a>
-            </li>
-            <li>
-              <a href='#'>Shopping Bag (0)</a>
-            </li>
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              <li>
+                <a href='#'>Wishlist</a>
+              </li>
+              <li>
+                <a href='#'>Account</a>
+              </li>
+              <li>
+                <a href='#'>Shopping Bag (0)</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <Home />
     </div>
