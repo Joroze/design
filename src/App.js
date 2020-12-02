28b42117aa3b0f9dfe11b354a671b50c6a846437
Jordan Rosenberg <joroze@Jordans-MacBook-Pro.local>
2020-12-02 13:26:09 -0500

    Fix styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,38 @@ import Home from 'pages/Home/Home';
 function App() {
   return (
     <div className="component-app">
-      <Home/>
+      <header className="app-nav">
+        <nav>
+          <ul>
+            <li>
+              <a href='#'>Products</a>
+            </li>
+            <li>
+              <a href='#'>Sale</a>
+            </li>
+            <li>
+              <a href='#'>Search</a>
+            </li>
+          </ul>
+        </nav>
+        <h2 className='title'>
+          Sculpture
+                </h2>
+        <nav>
+          <ul>
+            <li>
+              <a href='#'>Wishlist</a>
+            </li>
+            <li>
+              <a href='#'>Account</a>
+            </li>
+            <li>
+              <a href='#'>Shopping Bag (0)</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Home />
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -29,38 +29,6 @@ const sortMenuHeader = {
 function Home() {
     return (
         <div className="component-home">
-            <header className="home-menu">
-                <nav>
-                    <ul>
-                        <li>
-                            <a href='#'>Products</a>
-                        </li>
-                        <li>
-                            <a href='#'>Sale</a>
-                        </li>
-                        <li>
-                            <a href='#'>Search</a>
-                        </li>
-                    </ul>
-                </nav>
-                <h2 className='title'>
-                    Sculpture
-                </h2>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href='#'>Wishlist</a>
-                        </li>
-                        <li>
-                            <a href='#'>Account</a>
-                        </li>
-                        <li>
-                            <a href='#'>Shopping Bag (0)</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-
             <div className='container'>
                 {/* <Menu
                     name='Categories'
